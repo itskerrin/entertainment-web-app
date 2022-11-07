a585f677a285eb6638b0a514216d456de9a9e323
Kerrin Gorrie <kerrin.gorrie@hermes-europe.co.uk>
2022-11-07 10:56:52 +0000

    fetched all data from database

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,50 @@
-// import axios from 'axios';
-// import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
 import './main.scss';
 
 function App() {
+    const axios = require('axios');
+    const [moviesAndShows, setMoviesAndShows] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const moviesAndShowsURL = '#';
+    // 'https://spmyirwfx4.execute-api.us-east-1.amazonaws.com/dev/entertainment-app';
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await axios.get(moviesAndShowsURL);
+                console.log(response);
+                setMoviesAndShows(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        })();
+    }, []);
+
+    useEffect(() => {
+        moviesAndShows.length > 0 ? setIsLoading(false) : setIsLoading(false);
+    }, [moviesAndShows]);
+
+    useEffect(() => {
+        console.log(moviesAndShows);
+    }, [moviesAndShows]);
+
     return (
         <div className="App">
             <Router>
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    {/* <Route path="/admin" element={<Admin />} />
-                <Route
-                    path="/admin/trial"
-                    element={<AddEditTrial trials={trials} />}
-                />
-                <Route
-                    path="/admin/round"
-                    element={<AddEditRound trials={trials} rounds={rounds} />}
-                /> */}
+                    <Route
+                        path="/"
+                        element={
+                            <Home
+                                isLoading={isLoading}
+                                moviesAndShows={moviesAndShows}
+                            />
+                        }
+                    />
                 </Routes>
             </Router>
         </div>
--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,13 +1,42 @@
 import Nav from '../Nav';
 import SearchBar from '../SearchBar';
 import Trending from '../Trending';
+import { ThreeDots } from 'react-loader-spinner';
 
-const Home = () => {
+const Home = ({ isLoading }) => {
     return (
         <div>
-            <Nav />
-            <SearchBar />
-            <Trending />
+            {isLoading ? (
+                <div>
+                    <Nav />
+                    <ThreeDots
+                        height="700"
+                        width="100"
+                        radius="9"
+                        color="#5a698f"
+                        ariaLabel="three-dots-loading"
+                        wrapperStyle={{
+                            width: '100vw',
+                            display: 'flex',
+                            justifyContent: 'center',
+                        }}
+                        wrapperClassName="loader"
+                        id="loader"
+                        visible={isLoading}
+                    />
+                </div>
+            ) : (
+                <div>
+                    <Nav />
+                    <SearchBar />
+                    <div>
+                        <Trending />
+                    </div>
+                    {/* <div>
+                    
+                     </div> */}
+                </div>
+            )}
         </div>
     );
 };
