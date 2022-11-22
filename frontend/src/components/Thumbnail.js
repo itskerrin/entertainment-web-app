43ad719ac07e6c00d8270ca132dc82bf7b155cdf
Kerrin Gorrie <kerrin.gorrie@hermes-europe.co.uk>
2022-11-22 10:52:43 +0000

    added small thumbnails to grid, working on mobile layout

--- frontend/src/components/Thumbnail.js
+++ frontend/src/components/Thumbnail.js
@@ -1,14 +1,31 @@
 import medium from '../assets/medium.jpg';
 import './styles/components/_thumbnail.scss';
+import movieIcon from '../assets/icon-category-movie.svg';
 
 const Thumbnail = () => {
     return (
-        <div
-            id="thumbnail-regular"
-            style={{
-                backgroundImage: `url(${medium})`,
-            }}
-        ></div>
+        <>
+            <div
+                id="thumbnail-regular"
+                style={{
+                    backgroundImage: `url(${medium})`,
+                }}
+            ></div>
+            <div>
+                <div id="info-wrapper">
+                    <div id="info">
+                        <p>2002</p>
+                        <span>•</span>
+                        <p id="category">
+                            <img src={movieIcon} alt="movie" /> Movies
+                        </p>
+                        <span>•</span>
+                        <p>PG 13</p>
+                    </div>
+                    <div id="title">During the hunt</div>
+                </div>
+            </div>
+        </>
     );
 };
 
