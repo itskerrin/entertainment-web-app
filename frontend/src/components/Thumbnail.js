fee2ea69cfb9993811cdbb4813e82ef5dddf4103
kerrin <itskerrin@gmail.com>
2023-05-27 17:03:01 +0100

    added bookmark icons, working on localstorage

--- frontend/src/components/Thumbnail.js
+++ frontend/src/components/Thumbnail.js
@@ -1,43 +1,45 @@
+import { useEffect } from 'react';
 import movieIcon from '../assets/icon-category-movie.svg';
 import tvIcon from '../assets/icon-category-tv.svg';
+import emptyBookmark from '../assets/icon-bookmark-empty.svg';
 import './styles/components/_thumbnail.scss';
 
 const Thumbnail = ({ movieOrTVShow }) => {
-    return (
-        <>
-            <div id="thumbnail-regular">
-                <img
-                    src={movieOrTVShow.thumbnail.regular.medium}
-                    alt="thumbnail"
-                />
-            </div>
-            <div>
-                <div id="info-wrapper">
-                    <div id="info">
-                        <p>{movieOrTVShow.year}</p>
-                        <span>•</span>
-                        {movieOrTVShow.category === 'Movie' ? (
-                            <p id="category">
-                                <img src={movieIcon} alt="movie" /> Movie
-                            </p>
-                        ) : (
-                            <p id="category">
-                                <img
-                                    src={tvIcon}
-                                    alt="movie"
-                                    style={{ width: '40%' }}
-                                />
-                                TV
-                            </p>
-                        )}
-                        <span>•</span>
-                        <p>{movieOrTVShow.rating}</p>
-                    </div>
-                    <div id="title">{movieOrTVShow.title}</div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div id="thumbnail-regular">
+        <img
+          src={movieOrTVShow.thumbnail.regular.medium}
+          alt="thumbnail"
+          id="thumbnail"
+        />
+        <div id="bookmark-icon">
+          <img src={emptyBookmark} alt="Not bookmarked" />
+        </div>
+      </div>
+      <div>
+        <div id="info-wrapper">
+          <div id="info">
+            <p>{movieOrTVShow.year}</p>
+            <span>•</span>
+            {movieOrTVShow.category === 'Movie' ? (
+              <p id="category">
+                <img src={movieIcon} alt="movie" /> Movie
+              </p>
+            ) : (
+              <p id="category">
+                <img src={tvIcon} alt="movie" style={{ width: '40%' }} />
+                TV
+              </p>
+            )}
+            <span>•</span>
+            <p>{movieOrTVShow.rating}</p>
+          </div>
+          <div id="title">{movieOrTVShow.title}</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Thumbnail;
