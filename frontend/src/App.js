9cbee8cb1c435fd3a97eeff555046269172b5846
kerrin <itskerrin@gmail.com>
2023-05-27 15:36:42 +0100

    added trending section, updated grid size on desktop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,49 +5,42 @@ import Home from './components/pages/Home';
 import './main.scss';
 
 function App() {
-    const axios = require('axios');
-    const [moviesAndShows, setMoviesAndShows] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const axios = require('axios');
+  const [moviesAndShows, setMoviesAndShows] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const moviesAndShowsURL =
-        'https://spmyirwfx4.execute-api.us-east-1.amazonaws.com/dev/entertainment-app';
+  const moviesAndShowsURL =
+    'https://spmyirwfx4.execute-api.us-east-1.amazonaws.com/dev/entertainment-app';
 
-    useEffect(() => {
-        (async () => {
-            try {
-                const response = await axios.get(moviesAndShowsURL);
-                setMoviesAndShows(response.data);
-            } catch (error) {
-                console.error(error);
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get(moviesAndShowsURL);
+        setMoviesAndShows(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    moviesAndShows.length > 0 ? setIsLoading(false) : setIsLoading(true);
+  }, [moviesAndShows]);
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home isLoading={isLoading} moviesAndShows={moviesAndShows} />
             }
-        })();
-    }, []);
-
-    useEffect(() => {
-        moviesAndShows.length > 0 ? setIsLoading(false) : setIsLoading(true);
-    }, [moviesAndShows]);
-
-    useEffect(() => {
-        console.log(moviesAndShows);
-    }, [moviesAndShows]);
-
-    return (
-        <div className="App">
-            <Router>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            <Home
-                                isLoading={isLoading}
-                                moviesAndShows={moviesAndShows}
-                            />
-                        }
-                    />
-                </Routes>
-            </Router>
-        </div>
-    );
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -17,147 +17,119 @@ import '../styles/components/_nav.scss';
 import '../styles/utils/_layout.scss';
 
 const Home = ({ isLoading, moviesAndShows }) => {
-    const [content, setContent] = useState('home');
-    const [searchTerm, setSearchTerm] = useState('');
+  const [content, setContent] = useState('home');
+  const [searchTerm, setSearchTerm] = useState('');
 
-    return (
+  return (
+    <div>
+      {isLoading ? (
         <div>
-            {isLoading ? (
-                <div>
-                    <ThreeDots
-                        height="700"
-                        width="100"
-                        radius="9"
-                        color="#5a698f"
-                        ariaLabel="three-dots-loading"
-                        wrapperStyle={{
-                            width: '100vw',
-                            display: 'flex',
-                            justifyContent: 'center',
-                        }}
-                        wrapperClassName="loader"
-                        id="loader"
-                        visible={isLoading}
-                    />
-                </div>
-            ) : (
-                <div>
-                    <div id="nav-bar">
-                        <div>
-                            <img
-                                src={logo}
-                                id="logo"
-                                alt="logo"
-                                onClick={() => setContent('home')}
-                            />
-                        </div>
-                        <div id="nav-items">
-                            <img
-                                src={home}
-                                alt="home"
-                                onClick={() => setContent('home')}
-                            />
-                            <img
-                                src={movies}
-                                alt="movies"
-                                onClick={() => setContent('movies')}
-                            />
-                            <img
-                                src={tv}
-                                alt="tv"
-                                onClick={() => setContent('tv')}
-                            />
-                            <img
-                                src={bookmarks}
-                                alt="bookmarks"
-                                onClick={() => setContent('bookmarked')}
-                            />
-                        </div>
-                        <div id="account">
-                            <img src={avatar} id="avatar" alt="avatar" />
-                        </div>
-                    </div>
-
-                    <div id="search-container">
-                        <div id="search-icon">
-                            <img src={search} alt="search" />
-                        </div>
-                        <div id="search-form">
-                            <form>
-                                <input
-                                    type="text"
-                                    name="search"
-                                    placeholder="Search for movies or TV series"
-                                    onChange={(e) =>
-                                        setSearchTerm(e.target.value)
-                                    }
-                                />
-                            </form>
-                        </div>
-                    </div>
-                    {/* main content */}
-                    {content === 'movies' ? (
-                        <Movies
-                            moviesAndShows={moviesAndShows}
-                            searchTerm={searchTerm}
-                        />
-                    ) : content === 'tv' ? (
-                        <TVShows
-                            moviesAndShows={moviesAndShows}
-                            searchTerm={searchTerm}
-                        />
-                    ) : content === 'bookmarked' ? (
-                        <Bookmarked
-                            moviesAndShows={moviesAndShows}
-                            searchTerm={searchTerm}
-                        />
-                    ) : (
-                        <div id="home-container">
-                            <div>
-                                <h1 id="trending-title">Trending</h1>
-                                <div id="scrolling-container">
-                                    {moviesAndShows.map((trending, idx) => (
-                                        <ThumbnailLarge
-                                            trending={trending}
-                                            key={idx}
-                                        />
-                                    ))}
-                                </div>
-                            </div>
-                            <div>
-                                <h1 id="recommended-title">
-                                    Recommended for you
-                                </h1>
-                                <div id="main-content">
-                                    {moviesAndShows
-                                        .filter((result) => {
-                                            return searchTerm.toLowerCase() ===
-                                                ''
-                                                ? result
-                                                : result.title
-                                                      .toLowerCase()
-                                                      .includes(searchTerm);
-                                        })
-                                        .map((movieOrTVShow, idx) => (
-                                            <div
-                                                className="grid-item"
-                                                key={idx}
-                                            >
-                                                <Thumbnail
-                                                    movieOrTVShow={
-                                                        movieOrTVShow
-                                                    }
-                                                />
-                                            </div>
-                                        ))}
-                                </div>
-                            </div>
-                        </div>
-                    )}
-                </div>
-            )}
+          <ThreeDots
+            height="700"
+            width="100"
+            radius="9"
+            color="#5a698f"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{
+              width: '100vw',
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+            wrapperClassName="loader"
+            id="loader"
+            visible={isLoading}
+          />
         </div>
-    );
+      ) : (
+        <div>
+          <div id="nav-bar">
+            <div>
+              <img
+                src={logo}
+                id="logo"
+                alt="logo"
+                onClick={() => setContent('home')}
+              />
+            </div>
+            <div id="nav-items">
+              <img src={home} alt="home" onClick={() => setContent('home')} />
+              <img
+                src={movies}
+                alt="movies"
+                onClick={() => setContent('movies')}
+              />
+              <img src={tv} alt="tv" onClick={() => setContent('tv')} />
+              <img
+                src={bookmarks}
+                alt="bookmarks"
+                onClick={() => setContent('bookmarked')}
+              />
+            </div>
+            <div id="account">
+              <img src={avatar} id="avatar" alt="avatar" />
+            </div>
+          </div>
+
+          <div id="search-container">
+            <div id="search-icon">
+              <img src={search} alt="search" />
+            </div>
+            <div id="search-form">
+              <form>
+                <input
+                  type="text"
+                  name="search"
+                  placeholder="Search for movies or TV series"
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+              </form>
+            </div>
+          </div>
+          {/* main content */}
+          {content === 'movies' ? (
+            <Movies moviesAndShows={moviesAndShows} searchTerm={searchTerm} />
+          ) : content === 'tv' ? (
+            <TVShows moviesAndShows={moviesAndShows} searchTerm={searchTerm} />
+          ) : content === 'bookmarked' ? (
+            <Bookmarked
+              moviesAndShows={moviesAndShows}
+              searchTerm={searchTerm}
+            />
+          ) : (
+            <div id="home-container">
+              <div>
+                <h1 id="trending-title">Trending</h1>
+                <div id="scrolling-container">
+                  {moviesAndShows.map(
+                    (trending, idx) =>
+                      trending.isTrending && (
+                        <ThumbnailLarge trending={trending} key={idx} />
+                      )
+                  )}
+                </div>
+              </div>
+              <div>
+                <h1 id="recommended-title">Recommended for you</h1>
+                <div id="main-content">
+                  {moviesAndShows
+                    .filter((result) => {
+                      return searchTerm.toLowerCase() === ''
+                        ? result
+                        : result.title.toLowerCase().includes(searchTerm);
+                    })
+                    .map((movieOrTVShow, idx) => (
+                      <div className="grid-item" key={idx}>
+                        <Thumbnail movieOrTVShow={movieOrTVShow} />
+                      </div>
+                    ))}
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
